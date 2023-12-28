4679d1701c08264c8b27920fe133fb24d046b3af
Harsh-git1 <130690167+Harsh-git1@users.noreply.github.com>
2023-12-29 02:36:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstWord(s) {
   // your code here
+	let arr = s.split(" ");
+	let ans; 
+
+	for(int i=0; i<arr.length; i++) {
+		if(arr[i] != "") return arr[i];
+	}
+	return "";
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
-alert(firstWord(s));
+alert(firstWord(s)); 
